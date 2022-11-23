9e8879f135f8a92c24fb00796062b752709ebe69
Evgeni Kalukov <sneginkafire@yandex.ru>
2022-11-23 14:23:42 +0400

    2

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
 export const Categories = () => {
-	return (
-		<div className="categories">
-			<ul>
-				<li className="active">Все</li>
-				<li>Мясные</li>
-				<li>Вегетарианская</li>
-				<li>Гриль</li>
-				<li>Острые</li>
-				<li>Закрытые</li>
-			</ul>
-		</div>
-	);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+  const categories = [
+    "все",
+    "Мясная",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? "active" : ""}
+            key={i}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default Categories;
