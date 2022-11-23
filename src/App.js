8c7401b2fe8f86a8a576e6217cb7e191993489bb
Evgeni Kalukov <sneginkafire@yandex.ru>
2022-11-23 21:16:09 +0400

    5

--- src/App.js
+++ src/App.js
@@ -4,9 +4,40 @@ import Header from "./components/Header";
 import Sort from "./components/Sort";
 import Categories from "./components/Categories";
 import Pizzablock from "./components/Pizzablock";
-import pizzas from './assets/pizzas.json';
-console.log(pizzas);
+// import pizzas from './assets/pizzas.json';
+
 function App() {
+	 const pizzas= [
+		{
+		  "id": 0,
+		  "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg",
+		  "title": "Пепперони Фреш с перцем",
+		  "types": [0, 1],
+		  "sizes": [26, 30, 40],
+		  "price": 803,
+		  "category": 0,
+		  "rating": 4
+		}, {
+			"id": 1,
+			"imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+			"title": "Сырная",
+			"types": [0],
+			"sizes": [26, 40],
+			"price": 245,
+			"category": 1,
+			"rating": 6
+		 },
+		 {
+			"id": 2,
+			"imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/6652fec1-04df-49d8-8744-232f1032c44b.jpg",
+			"title": "Цыпленок барбекю",
+			"types": [0],
+			"sizes": [26, 40],
+			"price": 295,
+			"category": 1,
+			"rating": 4
+		 },]
+		console.log(pizzas);
 	return (
 		<div className="wrapper">
 			<Header />
@@ -18,9 +49,12 @@ function App() {
 					</div>
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
-						{pizzas.map((obj) => (
-						<Pizzablock title={obj.title} price={350} />
-						))	}				
+					
+					{
+						pizzas.map((obj) =>(
+							<Pizzablock  {...obj}/>
+						))
+					}
 					</div>
 				</div>
 			</div>
