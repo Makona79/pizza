893c43e0dc8f01810d6e9aa484a3703b89f3ee74
Evgeni Kalukov <sneginkafire@yandex.ru>
2022-11-26 18:34:09 +0400

    77

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,10 +15,11 @@ const Home = () => {
         setItems(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -29,7 +30,7 @@ const Home = () => {
           ? [...new Array(4)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <Pizzablock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
 
